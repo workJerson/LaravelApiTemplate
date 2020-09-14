206c1f91bb3cefc5a3bddcd79e887a2499e4affa
work.jerson.baltazar <work.jerson.baltazar@gmail.com>
2020-09-14 20:38:50 +0800

    WIP on relationships

---
+++ app/Models/CmsProfile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Filterable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CmsProfile extends Model
+{
+    use HasFactory;
+    use Filterable;
+}
