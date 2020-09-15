b570228b8475b549a5190bfee9bd8652e4cee325
work.jerson.baltazar <work.jerson.baltazar@gmail.com>
2020-09-16 00:10:10 +0800

    Added new controllers

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class AuthController extends Controller
 {
     /**
      * Display a listing of the resource.
