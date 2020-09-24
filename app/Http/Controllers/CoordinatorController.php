99232ce3c6b4124c1d8de5b3a7eb33775aa228a5
work.jerson.baltazar <work.jerson.baltazar@gmail.com>
2020-09-24 22:39:15 +0800

    Added relationship load for coordinator

--- app/Http/Controllers/CoordinatorController.php
+++ app/Http/Controllers/CoordinatorController.php
@@ -16,7 +16,7 @@ class CoordinatorController extends Controller
     public function index(ResourceFilters $filters, Coordinator $coordinator)
     {
         return $this->generateCachedResponse(function () use ($filters, $coordinator) {
-            $coordinators = $coordinator->filter($filters);
+            $coordinators = $coordinator->with(['user', 'hub'])->filter($filters);
 
             return $this->paginateOrGet($coordinators);
         });
