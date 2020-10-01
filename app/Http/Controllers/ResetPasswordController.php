8941fc98169cc7566939561b8a7c79a06102388a
work.jerson.baltazar <work.jerson.baltazar@gmail.com>
2020-10-01 21:14:32 +0800

    Done and tested reset password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 
@@ -21,9 +20,8 @@ class ResetPasswordController extends Controller
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function ($user, $password) use ($request) {
-                $user->forceFill([
-                    'password' => Hash::make($password),
-                ])->save();
+                $user->password = $password;
+                $user->save();
 
                 $user->setRememberToken(Str::random(60));
 
